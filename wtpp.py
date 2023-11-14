e53a9fcf2607e5cb72a12ab3291c6645a5e4e809
everduvan <everduvanhdez@gmail.com>
2023-11-14 10:04:15 -0500

    update

--- wtpp.py
+++
@@ -1,8 +0,0 @@
-import pywhatkit
-
-# para enviar un mensaje de whatsap a una hora especifica
-# pywhatkit.sendwhatmsg("+573043216917", "Hola", 12, 00)
-
-# para enviar un mensaje instantaneo
-pywhatkit.sendwhatmsg_instantly("+573013367420", "care verga")
-
