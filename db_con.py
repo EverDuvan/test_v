3b98f006e3548e83c0e650a8cea67a221f739845
everduvan <everduvanhdez@gmail.com>
2023-11-14 10:02:06 -0500

    update

---
+++ db_con.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from sqlalchemy.engine import create_engine
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def get_df(db: str, table, column=[]):
+    """
+    [get dataframe from postgres]
+    return: pandas-df
+    """
+    a = eval(os.getenv("DB"))
+    db=a[db]
+    engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format
+                           (db['DB_USER'], 
+                            db['DB_PASS'], 
+                            db['DB_IP'], 
+                            db['DB_PORT'], 
+                            db['DB_NAME']))
+    query= f'SELECT * from {table}'
+    df = pd.read_sql_query(query, engine)
+    if column != []:
+        df=df[column]
+    return df
+
+def send_df_append(dataframe ,db , table):
+    a = eval(os.getenv("DB"))
+    db=a[db]
+    engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format
+                            (db['DB_USER'], 
+                                db['DB_PASS'], 
+                                db['DB_IP'], 
+                                db['DB_PORT'], 
+                                db['DB_NAME']))
+    dataframe.to_sql(table, 
+                        engine, 
+                        schema='public', 
+                        if_exists='append', 
+                        index=False, 
+                        chunksize = 10)
+    print ('¡Done!')
+
+def send_df_replace(dataframe, db, table):
+    a = eval(os.getenv("DB"))
+    db=a[db]
+    engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format
+                            (db['DB_USER'], 
+                                db['DB_PASS'], 
+                                db['DB_IP'], 
+                                db['DB_PORT'], 
+                                db['DB_NAME']))
+    dataframe.to_sql(table,
+                        engine,
+                        schema='public',
+                        if_exists='replace',
+                        index=False,
+                        chunksize = 10)
+    print ('¡Done!')
+
+print(get_df('data_procesing2','homologados', ['URL','ID']))
---
+++ hitch_proxies.py
@@ -0,0 +1,65 @@
+import concurrent.futures
+import pandas as pd
+import requests
+import os
+from datetime import datetime
+import time
+
+# Start counting.
+start_time = time.time()
+
+# URL File path
+urls_file = 'urls.xlsx'
+# Proxies File path
+proxies_file = 'proxies.txt'
+
+# Reading proxies
+with open(proxies_file, 'r') as f:
+    proxies = [line.strip() for line in f]
+# Reading URLs
+df_urls = pd.read_excel(urls_file)
+
+# create DF columns = ['proxies', 'working'] + df_urls.columns
+df = pd.DataFrame(columns=['proxies', 'working'] + df_urls.columns.tolist())
+
+def proxy_check(proxy):
+    # Add 'http://' if not exist
+    if not proxy.startswith('http://'):
+        proxy = 'http://' + proxy
+    for _, row in df_urls.iterrows():
+        url = row['urlCategory']
+        try:
+            response = requests.get(url, proxies={'http': proxy, 'https': proxy}, timeout=5)
+            if response.status_code == 200:
+                print(f'Proxy {proxy} working with URL {url}')
+                # add working status to DF
+                df.loc[len(df)] = [proxy.replace("http://", ""), True] + row.tolist()
+            else:
+                print(f'Proxy {proxy} not working with URL {url}')
+                # add working status to DF
+                df.loc[len(df)] = [proxy.replace("http://", ""), False] + row.tolist()
+        except Exception as e:
+            print(f'Proxy {proxy} not working with URL {url}')
+            # add working status to DF
+            df.loc[len(df)] = [proxy.replace("http://", ""), False] + row.tolist()
+    return df
+
+# Number of threads
+num_threads = 4
+
+with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+    executor.map(proxy_check, proxies)
+
+# Actual date time 
+now = datetime.now()
+now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
+# Making the folder 'proxies_test' if it doesn't exist
+if not os.path.exists('proxies_test'):
+    os.makedirs('proxies_test')
+# save DataFrame to Excel
+df.to_excel(f'proxies_test/{now_str}.xlsx', index=False)
+
+# Calculate the elapsed time.
+elapsed_time = (time.time() - start_time)/60
+print(f'Elapsed time in total: {round(elapsed_time, 2)} Minutes.')
+print(f"DF correctly saved in 'proxies_test/{now_str}.xlsx'")
