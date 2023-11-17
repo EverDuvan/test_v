24464589d717b9f08439f1960d1d2977771775d3
everduvan <everduvanhdez@gmail.com>
2023-11-17 10:44:11 -0500

    update

--- func.py
+++ func.py
@@ -2,7 +2,36 @@ import pandas as pd
 from sqlalchemy.engine import create_engine
 from dotenv import load_dotenv
 import os
+import requests
+import time
+import threading
 load_dotenv()
+df_lock = threading.Lock()
+
+def proxy_check(proxy, df, df_urls):
+    # Add 'http://' if not exist
+    if not proxy.startswith('http://'):
+        proxy = 'http://' + proxy
+    for _, row in df_urls.iterrows():
+        url = row['url_category']
+        try:
+            response = requests.get(url, proxies={'http': proxy, 'https': proxy}, timeout=5)
+            if response.status_code == 200:
+                print(f'Proxy {proxy} working with URL {url}')
+                with df_lock:
+                # add working status to DF
+                    df.loc[len(df)] = [proxy.replace("http://", ""), True] + row.tolist()
+            else:
+                print(f'Proxy {proxy} not working with URL {url}')
+                # add working status to DF
+                with df_lock:
+                    df.loc[len(df)] = [proxy.replace("http://", ""), False] + row.tolist()
+        except Exception as e:
+            print(f'Proxy {proxy} not working with URL {url}')
+            # add working status to DF
+            with df_lock:
+                df.loc[len(df)] = [proxy.replace("http://", ""), False] + row.tolist()
+    return df
 
 def get_df(db: str, table, column=[]):
     """
--- main.py
+++ main.py
@@ -1,69 +1,45 @@
 import concurrent.futures
 import pandas as pd
-import requests
 import os
 from datetime import datetime
 import time
-import threading
-from db_con import *
+from func import *
 
-# Start counting.
-start_time = time.time()
+def main():
+    # Start counting.
+    start_time = time.time()
 
-df_urls = get_df('proxies_test', 'urls')
-df_proxies= get_df('proxies_test', 'proxies')
-proxies = df_proxies['ip'].tolist()
+    df_urls = get_df('proxies_test', 'urls')
+    df_proxies= get_df('proxies_test', 'proxies')
+    proxies = df_proxies['ip'].tolist()
 
-# create DF columns = ['proxies', 'working'] + df_urls.columns
-df = pd.DataFrame(columns=['proxies', 'working'] + df_urls.columns.tolist())
+    # create DF columns = ['proxies', 'working'] + df_urls.columns
+    df = pd.DataFrame(columns=['proxies', 'working'] + df_urls.columns.tolist())
 
-df_lock = threading.Lock()
+    # Number of threads
+    num_threads = 4
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        # Utilizar función partial para pasar parámetros adicionales
+        partial_proxy_check = lambda proxy: proxy_check(proxy, df, df_urls)
+        executor.map(partial_proxy_check, proxies)
 
-def proxy_check(proxy):
-    # Add 'http://' if not exist
-    if not proxy.startswith('http://'):
-        proxy = 'http://' + proxy
-    for _, row in df_urls.iterrows():
-        url = row['url_category']
-        try:
-            response = requests.get(url, proxies={'http': proxy, 'https': proxy}, timeout=5)
-            if response.status_code == 200:
-                print(f'Proxy {proxy} working with URL {url}')
-                with df_lock:
-                # add working status to DF
-                    df.loc[len(df)] = [proxy.replace("http://", ""), True] + row.tolist()
-            else:
-                print(f'Proxy {proxy} not working with URL {url}')
-                # add working status to DF
-                with df_lock:
-                    df.loc[len(df)] = [proxy.replace("http://", ""), False] + row.tolist()
-        except Exception as e:
-            print(f'Proxy {proxy} not working with URL {url}')
-            # add working status to DF
-            with df_lock:
-                df.loc[len(df)] = [proxy.replace("http://", ""), False] + row.tolist()
-    return df
+    # Actual date time 
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
+    # Making the folder 'proxies_test' if it doesn't exist
+    if not os.path.exists('proxies_test'):
+        os.makedirs('proxies_test')
 
-# Number of threads
-num_threads = 4
+    # save DataFrame to DB
+    send_df_replace(df, 'proxies_test', 'result')
 
-with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-    executor.map(proxy_check, proxies)
+    # save DataFrame to Excel
+    df.to_excel(f'proxies_test/{now_str}.xlsx', index=False)
 
-# Actual date time 
-now = datetime.now()
-now_str = now.strftime("%Y-%m-%d_%H-%M-%S")
-# Making the folder 'proxies_test' if it doesn't exist
-if not os.path.exists('proxies_test'):
-    os.makedirs('proxies_test')
+    # Calculate the elapsed time.
+    elapsed_time = (time.time() - start_time)/60
+    print(f'Elapsed time in total: {round(elapsed_time, 2)} Minutes.')
+    print(f"DF correctly saved in 'proxies_test/{now_str}.xlsx'")
 
-# save DataFrame to DB
-send_df_replace(df, 'proxies_test', 'result')
-
-# save DataFrame to Excel
-df.to_excel(f'proxies_test/{now_str}.xlsx', index=False)
-
-# Calculate the elapsed time.
-elapsed_time = (time.time() - start_time)/60
-print(f'Elapsed time in total: {round(elapsed_time, 2)} Minutes.')
-print(f"DF correctly saved in 'proxies_test/{now_str}.xlsx'")
+if __name__ == "__main__":
+    main()
